b49eaabc582c8f65c5a1645c274e5829f3a5ef9f
maaku33 <mareksmolarczyk33@gmail.com>
2017-11-22 14:17:26 +0100

    Działające konstruktory ApplePie i CherryPie

--- pie.h
+++ pie.h
@@ -3,41 +3,47 @@
 
 #include <type_traits>
 
-template <typename T>
+template <int>
 struct is_cherry {
-    bool value = false;
+    const static bool value = false;
 };
 
 template <>
-struct is_cherry<CherryPie> {
-    bool value = true;
+struct is_cherry<1> {
+    const static bool value = true;
 };
 
-template <typename T>
+template <int>
 struct is_apple {
-    bool value = false;
+    const static bool value = false;
 };
 
 template <>
-struct is_apple<ApplePie> {
-    bool value = true;
+struct is_apple<2> {
+    const static bool value = true;
 };
 
-template <typename R, R radius, typename P, typename T>
+template <typename R, R radius, typename P, int type>
 class Pie {
 
     int initialStock;
     P price;
 
 public:
-    void CherryPie(typename std::enable_if<is_cherry<T>, int>::type initialStock)
-        : initialStock(initialStock) {};
+    template <int t = type>
+    Pie(int initialStock, typename std::enable_if<is_cherry<t>::value>::type* = nullptr):
+        initialStock(initialStock) {};
 
-    void ApplePie(typename std::enable_if<is_apple<T>, int>::type initialStock, P price)
-        : initialStock(initialStock), price(price) {};
+    template <int t = type>
+    Pie(int initialStock, P price, typename std::enable_if<is_apple<t>::value>::type* = nullptr):
+        initialStock(initialStock),
+        price(price) {};
 };
 
-using CherryPie = Pie <R, R radius, double, CherryPie>;
-using ApplePie = Pie <R, R radius, P, ApplePie>;
+template <typename R, R radius>
+using CherryPie = Pie<R, radius, double, 1>;
+
+template <typename R, R radius, typename P>
+using ApplePie = Pie<R, radius, P, 2>;
 
 #endif /* __PIE_H__ */
