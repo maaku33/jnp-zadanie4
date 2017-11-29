c607d6442a1be04d3859656cf3df31b688bef7e5
maaku33 <mareksmolarczyk33@gmail.com>
2017-11-29 21:20:32 +0100

    Pełna implementacja klasy Cake i jej test

--- cake.h
+++ cake.h
@@ -11,27 +11,7 @@ constexpr double ln2_(unsigned n, unsigned i = 0) {
 // ? digit precision of natural log of 2
 constexpr double ln2 = ln2_(0);
 
-template <int>
-struct is_cheese {
-    const static bool value = false;
-};
-
-template <>
-struct is_cheese<1> {
-    const static bool value = true;
-};
-
-template <int>
-struct is_cream {
-    const static bool value = false;
-};
-
-template <>
-struct is_cream<2> {
-    const static bool value = true;
-};
-
-template <typename T, T length, T width, typename P, int type>
+template <typename T, T length, T width, typename P, bool sellable>
 class Cake {
 
     static_assert(std::is_integral<T>::value,
@@ -43,15 +23,45 @@ class Cake {
     const P price;
 
 public:
-    template <int t = type, typename std::enable_if<is_cheese<t>::value, int>::type = 0>
+    template <bool s = sellable, typename std::enable_if<!s, int>::type = 0>
     Cake(int initialStock): stock(initialStock), price() {
         assert(initialStock > 0);
     }
 
-    template <int t = type, typename std::enable_if<is_cream<t>::value, int>::type = 0>
+    template <bool s = sellable, typename std::enable_if<s, int>::type = 0>
     Cake(int initialStock, P price): stock(initialStock), price(price) {
         assert(initialStock > 0);
     }
+
+    static double getArea() {
+        return ln2 * length * width;
+    }
+
+    int getStock() {
+        return stock;
+    }
+
+    template <typename ... Buffer, bool s = sellable>
+    typename std::enable_if<s, void>::type sell() {
+        static_assert(sizeof ... (Buffer) == 0,
+            "Template arguments should not be specified.");
+
+        stock = stock - (stock > 0 ? 1 : 0);
+    }
+
+    template <typename ... Buffer, bool s = sellable>
+    typename std::enable_if<s, P>::type getPrice() {
+        static_assert(sizeof ... (Buffer) == 0,
+            "Template arguments should not be specified.");
+
+        return price;
+    }
 };
+
+template <typename T, T length, T width>
+using CheeseCake = Cake<T, length, width, double, false>;
+
+template <typename T, T length, T width, typename P>
+using CreamCake = Cake<T, length, width, P, true>;
 
 #endif /* __CAKE_H__ */
---
+++ cake_test1.cc
@@ -0,0 +1,9 @@
+#include "cake.h"
+#include <iostream>
+
+int main() {
+    CheeseCake<int, 10, 15> cheeseCake1(4);
+    CreamCake<int, 12, 12, double> creamCake(3, 25.9);
+
+    
+}
